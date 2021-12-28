15db29552bafb602a1b06fc0cda4c7a588d4be7b
Pernilla <coolanilla@hotmail.com>
2021-12-28 13:27:47 +0100

    Created GameBoard and fixed some styling on HomePage and GameTypeCard

---
+++ src/components/GameBoard.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const GameBoard = ({ gameType }) => {
+    return (
+        <div className="game-board">GameBoard - {gameType}</div>
+    );
+}
+ 
+export default GameBoard;
--- src/pages/GamePage.jsx
+++ src/pages/GamePage.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import GameBoard from '../components/GameBoard';
 
 const GamePage = () => {
     const { gameType } = useParams();
     
     return (
         <div className="game-page">
-            {gameType}
+            <GameBoard gameType={gameType} />
         </div>
     );
 }
