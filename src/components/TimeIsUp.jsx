af10de0365740c589cae9afb58f5e33d058d630f
Pernilla <coolanilla@hotmail.com>
2022-05-10 16:21:13 +0200

    Small changes to time is up component

--- src/components/TimeIsUp.jsx
+++ src/components/TimeIsUp.jsx
@@ -29,23 +29,23 @@ const TimeIsUp = ({ setShowTimeIsUp, score, previousHighScore, startGame }) => {
                     <>
                         {score > previousHighScore ? (
                             <>
-                                <p>You beat you high score!</p>
+                                <p className="result-text">You beat your high score!</p>
                                 <p className="score-title">New high score</p>
                                 <p className="total-score">{score}</p>
                             </>
                         ) : (
                             <>
-                                <p>You didn't beat your high score</p>
+                                <p className="result-text">You didn't beat your high score</p>
                                 <p className="score-title">Score</p>
                                 <p className="total-score">{score}</p>
                             </>
                         )}
+                        <div className="button-container">
+                            <div className="answer-button" onClick={handlePlayAgainClick}>Play again</div>
+                            <div className="answer-button" onClick={handleBackClick}>Back to home</div>
+                        </div>
                     </>
                 )}
-                <div className="button-container">
-                    <div className="answer-button" onClick={handlePlayAgainClick}>Play again</div>
-                    <div className="answer-button" onClick={handleBackClick}>Back to home</div>
-                </div>
             </div>
         </div>
     );
